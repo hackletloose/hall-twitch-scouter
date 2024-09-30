c24a17d5887822464363e75fac08c062a2ff2240
Walter Röhrl <r.roeder@screenus.de>
2024-09-30 17:06:11 +0200

    Update twitch-scouter.py

--- twitch-scouter.py
+++ twitch-scouter.py
@@ -20,12 +20,8 @@ from bin.connection_twitch import (
 )
 from bin.connection_discord import (
     MyView,
-<<<<<<< HEAD
     CategorizeModal,
     PlayerSearchModal,
-=======
-    UnifiedModal,
->>>>>>> 50cc472ee59ad5b95636ab9771efe561f00d65ee
     send_unwanted_report
 )
 
